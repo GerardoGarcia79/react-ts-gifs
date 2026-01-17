74dcd419bc3abc9b1191650408b3d08713cf9fe1
Gerardo de Leon Garcia <gerardodlg.dev@gmail.com>
2026-01-16 19:41:20 -0600

    feat: create counter app

---
+++ src/counter/MyCounterApp.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export const MyCounterApp = () => {
+  const [counter, setCounter] = useState(5);
+
+  const handleAdd = () => setCounter((prev) => prev + 1);
+
+  const handleSubtract = () => setCounter((prev) => prev - 1);
+
+  const handleReset = () => setCounter(5);
+
+  return (
+    <div
+      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+    >
+      <h1>Counter: {counter}</h1>
+
+      <div style={{ display: "flex", gap: "10px" }}>
+        <button onClick={handleAdd}>+1</button>
+        <button onClick={handleSubtract}>-1</button>
+        <button onClick={handleReset}>Reset</button>
+      </div>
+    </div>
+  );
+};
